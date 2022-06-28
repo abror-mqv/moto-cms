de9c710dfe9244ea53827fd9c0a561cbebc5513e
Abror <mmtklvabrr@gmail.com>
2022-06-28 21:22:25 +0600

    erfew

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,24 @@
-const parse = require('pg-connection-string').parse
-const config = parse(process.env.DATABASE_URL)
-
 module.exports = ({ env }) => ({
-    connection: {
-        client: 'postgress',
-        connection: {
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            user: config.user,
-            password: config.password,
-            ssl: {
-                rejectUnauthorized: false
-            }
-
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: env('DATABASE_HOST', '127.0.0.1'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'strapi'),
+          username: env('DATABASE_USERNAME', ''),
+          password: env('DATABASE_PASSWORD', ''), 
+          //add this line
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+          },     
         },
-        debug: false
-    }
-})
+        // add this line
+        options: {
+          ssl: env.bool('DATABASE_SSL', false),
+        },
+      },
+    },
+  });
